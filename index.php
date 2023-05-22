c1194cdee1b6b7a70b6dd2b2175bc6f6159e7075
romain-larue <61936818+romain-larue@users.noreply.github.com>
2023-05-22 09:53:54 +0200

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mon site web</title>
+    <style>
+        /* Style du bandeau */
+        #bandeau {
+            background-color: black;
+            height: 50px;
+        }
+        
+        /* Style des liens dans le bandeau */
+        #bandeau a {
+            color: white;
+            text-decoration: none;
+            margin-right: 20px;
+            line-height: 50px;
+        }
+    </style>
+</head>
+<body>
+    <div id="bandeau">
+        <a href="accueil.php">Accueil</a>
+        <a href="presentation.php">Présentation</a>
+        <a href="projet1.php">Projet 1</a>
+        <a href="projet2.php">Projet 2</a>
+    </div>
+    
+    <!-- Le reste du contenu de votre page -->
+    
+</body>
+</html>
